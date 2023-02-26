1285d804e7e83df6673cfb4dfc305d01977488f4
eshamibrahim94@gmail.com <eshamibrahim94@gmail.com>
2023-02-26 21:57:27 +0800

    Installing package (Body parser) and implementing sample body parser and CORS countermeasure

--- app.js
+++ app.js
@@ -3,11 +3,25 @@ const app = express();
 
 // middleware for log all http request
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Bypassing the CORS error
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-Width, Content-Type, Accept, Authorization')
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, DELETE, PATCH, GET')
+    }
+    next();
+});
+
 app.use('/products', productRoutes)
 app.use('/orders', orderRoutes)
 
